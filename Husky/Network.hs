3f713c31067d87060674431f4ad00a5e6263a4b7
abaw <abawwu@gmail.com>
2011-12-02 22:25:19 +0800

    1. don't list loopback address(127.0.0.1)

--- Husky/Network.hs
+++ Husky/Network.hs
@@ -6,10 +6,14 @@ module Husky.Network
 
 import Network.Info
 import Control.Monad
+import Data.Bits
+import Data.Binary
+import Data.Binary.Put
 
 
 -- | return all IPv4 address of local computer
 allHostAddrs :: IO [String]
-allHostAddrs = (map show . filter notAllZero . map ipv4)  `liftM` getNetworkInterfaces
+allHostAddrs = (map show . filter wanted . map ipv4)  `liftM` getNetworkInterfaces
                where
-                 notAllZero (IPv4 w) = w /= 0
+                 wanted (IPv4 w) = w /= 0 && w/= loopback
+		 loopback = decode $ runPut $ putWord32host $ shift 127 24 + 1
